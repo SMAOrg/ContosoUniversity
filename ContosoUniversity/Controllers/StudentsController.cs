217416c00466128122a2359acf70737b76b792d4
SlawekM <slawomir.malyszko@gmail.com>
2024-11-03 22:25:10 +0100

    part2_delete_updated

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -131,18 +131,26 @@ public class StudentsController : Controller
     }
 
     // GET: Students/Delete/5
-    public async Task<IActionResult> Delete(int? id)
+    public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
     {
-        if (id == null)
+        if (id is null)
         {
             return NotFound();
         }
 
         var student = await _context.Students
+            .AsNoTracking()
             .FirstOrDefaultAsync(m => m.ID == id);
-        if (student == null)
+        if (student is null)
         {
             return NotFound();
+        }
+
+        if (saveChangesError.GetValueOrDefault())
+        {
+            ViewData["ErrorMessage"] =
+                "Delete failed. Try again, and if the problem persists " +
+                "see your system administrator.";
         }
 
         return View(student);
@@ -154,13 +162,22 @@ public class StudentsController : Controller
     public async Task<IActionResult> DeleteConfirmed(int id)
     {
         var student = await _context.Students.FindAsync(id);
-        if (student != null)
+        if (student is null)
         {
-            _context.Students.Remove(student);
+            return RedirectToAction(nameof(Index));
         }
 
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
+        try
+        {
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        catch (DbUpdateException /* ex */)
+        {
+            // Log the error (uncomment ex variable name and write log.)
+            return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+        }
     }
 
     private bool StudentExists(int id)
